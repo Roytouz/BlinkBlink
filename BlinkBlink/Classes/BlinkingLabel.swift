f2ce1a75b58cbdac43f644f17812b1a2fb7438a4
Roy <roy.dourng@pathmazing.com>
2019-03-06 15:51:55 +0700

    zzz

--- BlinkBlink/Classes/BlinkingLabel.swift
+++ BlinkBlink/Classes/BlinkingLabel.swift
@@ -5,6 +5,13 @@
 import Foundation
 
 public class BlinkBlink {
+    
+    var generate:Any
+    public init(generated: Any){
+         self.generate = generated
+    }
+    
+    
     public func generateCode() {
         
         print("ˆ%ˆ&%ˆ&%&ˆ%&ˆ%&ˆ%&ˆˆ%&dashdashddhdh#$$#$%#$%$#%##$%#")
